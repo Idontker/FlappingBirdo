7ba3f5c9b54b587fc4690a3259f819b622f40482
idontker <idontker@users.noreply.github.com>
2023-07-18 09:37:05 +0200

    Initiales Game

---
+++ KameraManager.java
@@ -0,0 +1,14 @@
+
+/**
+ * Beschreiben Sie hier die Klasse KameraManager.
+ * 
+ * @author Karol Bakas
+ * @version 18.07.23
+ */
+public class KameraManager
+{
+    public KameraManager()
+    {
+    }
+
+}
